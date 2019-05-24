679d134cc59c733c0c9de1ec75520a7ecc284299
michalraj <michal_raj@o2.pl>
2019-05-24 14:36:18 +0200

    zadanie 10 strona 66 poczatek

---
+++ Project1/Project1/Źródło.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	string tekst;
+	int liczba_sp = 0;
+
+	cout << "Podaj tekst: ";
+
+	{
+		if (tekst[i] == ' ')
+			liczba_sp++;
+	}
+
+	cout << "Ilosc spacji: " << liczba_sp;
+
+
+	return 0;
+}
